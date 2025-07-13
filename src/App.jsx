1a6f1032a0249e433c35ca8470c34e8eb5621c6c
rajeev <rajeevkumar25112002@gmail.com>
2025-07-13 19:05:57 +0530

    feat: implement Footer component, set up Redux store and authentication slice

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,11 @@
-import React from 'react';
+import Footer from "./components/Footer";
+
+function App() {
+    return (  
+        <>
+            <Footer/>
+        </>
+    );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-
+import {Provider} from 'react-redux';
+import store from './Redux/store.js';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
      <App />
      
-  </BrowserRouter>
+    </BrowserRouter>
+  </Provider>
+  
     
  
 )
