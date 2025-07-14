ca307ba63bad559aae6b30423fbac997f4ef212c
rajeev <rajeevkumar25112002@gmail.com>
2025-07-14 20:20:06 +0530

    Add HomePage component with layout and content
    - Implemented HomePage component with a responsive layout.
    - Added main heading and description for online courses.
    - Included buttons for exploring courses and contacting us.
    - Integrated an image to enhance visual appeal.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import Footer from "./components/Footer";
+
+import { Route,Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
     return (  
         <>
-            <Footer/>
+        <Routes>
+            <Route path="/" element={<HomePage/>}></Route>
+        </Routes>
+           
         </>
     );
 }
